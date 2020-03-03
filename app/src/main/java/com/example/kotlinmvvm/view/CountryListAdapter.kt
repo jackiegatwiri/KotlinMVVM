fa12108833b03d6c7740c4bda0fb26939cc7c1c1
jackline <jgatwiri@innovexsolutions.co.ke>
2020-03-04 00:40:03 +0300

    kotlin mvvm dager rxjava retrofit

---
+++ app/src/main/java/com/example/kotlinmvvm/view/CountryListAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.kotlinmvvm.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinmvvm.R
+import com.example.kotlinmvvm.model.Country
+import kotlinx.android.synthetic.main.item_country.view.*
+
+class CountryListAdapter(var countries : ArrayList<Country>) : RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
+
+
+    //basically creates the CountryViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CountryViewHolder (
+        LayoutInflater.from(parent.context).inflate(R.layout.item_country, parent, false)
+
+    )
+
+    override fun getItemCount() = countries.size
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.bind(countries[position])
+    }
+
+    class CountryViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        //the view is basically the view that we have created on item_country
+        private val countryName = view.name
+        fun bind(country: Country){
+            countryName.text = country.countryName
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kotlinmvvm/viewmodel/ListViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.kotlinmvvm.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kotlinmvvm.model.Country
+
+//view model helps in creating liveData that users can subscribe to.. when the variable
+//changes then this subscribers are notified
+
+class ListViewModel:ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>() //basicall notify subscribers incase of an error
+    val loading = MutableLiveData<Boolean>() //tells if backend is in the process of loading countries
+
+    fun refresh()
+    {
+        fetchCountries()
+    }
+
+    //set your method for fetching data private then access it using refresh public
+    //here we are fetching data from backend, if data is successfull set error to false
+    //and loading to false and set mockdata tocountries then notify subscribers
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD")
+        )
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+ }
